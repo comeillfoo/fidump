b46cb24589046ebfdc076f9144347665ecf258c1
come_ill_foo <rayveleaveme34@inbox.ru>
2021-12-13 01:11:37 +0300

    feat: hacked-way to count digits in number ( thank to @Fant1k34 )

--- src/utils.h
+++ src/utils.h
@@ -3,16 +3,8 @@
 
 #include <linux/types.h>
 
-static inline size_t __do_get_length( u64 n ) {
-    if ( n == 0 )
-        return 0;
-    else return __do_get_length( n / 10 ) + 1;
-}
-
-
 static inline size_t get_length( u64 n ) {
-    const size_t length = __do_get_length( n );
-    return ( length == 0 )? 1 : length;
+    return snprintf( NULL, 0, "%llu", n );
 } 
 
 #endif /* _UTILS_H_ */
